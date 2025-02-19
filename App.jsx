bff6a3566402a00846b01cb873e0f7add4397bc1
KJONDIVE <galkin.anton2000@gmail.com>
2025-02-19 16:25:42 +0500

    first commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { registerRootComponent } from "expo";
+import { View, Text } from "react-native";
+
+function App() {
+  return (
+    <View>
+      <Text>HI!</Text>
+    </View>
+  );
+}
+
+registerRootComponent(App);
