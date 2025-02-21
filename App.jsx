a5e388bcf9e5f3c646adb4536d6849c22c3afaf2
KJONDIVE <galkin.anton2000@gmail.com>
2025-02-21 15:29:51 +0500

    chore: App: change status bar color to light

--- App.jsx
+++ App.jsx
@@ -1,14 +1,16 @@
 // *** IMPORT ***
 import { registerRootComponent } from "expo";
 import { Provider } from 'react-redux';
-import { store } from './src/redux/store';
+import { StatusBar } from "expo-status-bar";
 
 // *** OTHER ***
 import ProductListScreen from './src/screens/ProductListScreen';
+import { store } from './src/redux/store';
 
 export default function App() {
   return (
     <Provider store={store}>
+      <StatusBar style="light"></StatusBar>
       <ProductListScreen />
     </Provider>
   );
